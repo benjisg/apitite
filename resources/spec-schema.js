5b2dc017c039620a9b11d5987ac14b44e0b1fed6
Benji Schwartz-Gilbert <benji.schwartzgilbert@gmail.com>
2011-08-22 00:04:45 -0700

    Syntax fixes and temporarily removed JSON Schema validation

--- resources/spec-schema.js
+++ resources/spec-schema.js
@@ -1,10 +1,45 @@
 {
-	
-	
-	
-	
-	
-	
-	
-	
+	description : "The core API module specification",
+	type : "object",
+	properties : {
+		name : {
+			description : "Name that should be registered by apitite to call the API with",
+			type : "string",
+			minLength : 1,
+			maxLength : 100,
+			optional : false,
+			fallbacks : {
+				maxLength : "truncateToMaxLength"
+			}
+		},
+		version : {
+			description : "Version of the API this spec is detailing; if left blank then assumed to be latest major",
+			type : "string",
+			optional : true,
+			"default" : ""
+		},
+		services : {
+			description : "Provides a list of the REST queries each API provides",
+			type : "array",
+			minItems : 1,
+			items : {
+				{
+					description : "A REST service group",
+					type : "object",
+					properties : {
+						name : {
+							description : "Name of REST service group; if left blank then group is defaulted to global for the API",
+							type : "string",
+							maxLength : 100,
+							optional : false,
+							fallbacks : {
+								maxLength : "truncateToMaxLength"
+							}
+						}
+					}
+				}
+			}
+		}
+	},
+	additionalProperties : false
 }
